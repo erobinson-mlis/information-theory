b4e26fc2cb637e1171dda3c153896b7e80a241ba
Eric <163595322+erobinson-mlis@users.noreply.github.com>
2024-07-07 14:59:09 -0400

    Add files via upload

--- shannon-entropy.py
+++ shannon-entropy.py
@@ -1,6 +1,7 @@
 # shannon-entropy.py
 # calculates Shannon Entropy of a given a binary string
 
+import math
 
 
 #def shannon_entropy(x):
@@ -10,10 +11,10 @@
 #    return entropy
 
 
-
-
 def binary_string_check(string):
+
     ''' accepts a string and returns True if the string is composed only of 0s and 1s'''
+
     list = [x for x in string]
     not_binary = []
 
@@ -28,14 +29,28 @@ def binary_string_check(string):
     return is_binary
 
 
+def shannon_entropy(string):
+
+    '''Accepts a string (or maybe a list) and calculates the total entropy of the string.'''
+    
+    list = [x for x in string]
+    
+    for i in list:
+        H = (0.5 * math.log(2,2)) * len(list)
+    
+    return H
+
+
 string = input(f"Please enter a binary string of digits (0s and 1s only): ")
 
-if binary_string_check(string) == True:
-    print("This string contains only 0s and 1s. Proceed")
 if binary_string_check(string) == False:
     print("This string contains something that is not a 0 or 1.")
 
+if binary_string_check(string) == True:
+    print("This string contains only 0s and 1s. Proceed")
 
+shannon_entropy(string)
+print(f"The Shannon entropy of {string} is H = {shannon_entropy(string)}")
 
 
 
